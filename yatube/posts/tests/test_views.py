13a0c48f6bc1ac2b7ba24528ce4fac5c9cb7e1bf
Макеева Олеся <makeeva.o.a@yandex.ru>
2022-04-01 22:21:48 +0300

    12

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -153,7 +153,7 @@ class PostPagesTests(TestCase):
         """Новый пост попал в профиль."""
         response = self.authorized_client.get(reverse(self.PROFILE,
                                                       args=[USERNAME]))
-        posts = response.context['author']
+        posts = response.context['author'].username
         self.assertIn(self.post, posts)
 
     def test_post_not_in_your_group(self):
